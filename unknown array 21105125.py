e91f2461c1ba5cc349fd06cd1e0fbb46ec6cf9c7
shiv13anand <104129521+shiv13anand@users.noreply.github.com>
2022-10-02 01:22:56 +0530

    Update unknown array 21105125.py

--- unknown array 21105125.py
+++ unknown array 21105125.py
@@ -24,10 +24,10 @@ def binary_search(arr,l,r,x):
 
 	return -1
 
-# function takes an infinite size array(as the size is unknown) and a key to be searched and returns its position if found else -1. We don't know size of a[] and we can assume size to be infinite in this function.
-# NOTE THAT THIS FUNCTION ASSUMES a[] TO BE OF INFINITE SIZE
-#Let p be the position of element to be searched. Number of steps for finding high index ‘h’ is O(Log p).
-#'h’ must be less than 2*p. The number of elements between h/2 and h must be O(p). Therefore, time complexity of Binary Search step is also O(Log p) and overall time complexity is 2*O(Log p) which is O(Log p)
+function takes an infinite size array(as the size is unknown) and a key to be searched and returns its position if found else -1. We don't know size of a[] and we can assume size to be infinite in this function.
+ NOTE THAT THIS FUNCTION ASSUMES a[] TO BE OF INFINITE SIZE
+Let p be the position of element to be searched. Number of steps for finding high index ‘h’ is O(Log p).
+'h’ must be less than 2*p. The number of elements between h/2 and h must be O(p). Therefore, time complexity of Binary Search step is also O(Log p) and overall time complexity is 2*O(Log p) which is O(Log p)
 
 def findPos(a, key):
 
@@ -46,7 +46,7 @@ def findPos(a, key):
 # Driver function
     
 
-arr = [ 1,2,3,4,5,6,7,8,9,-1,-1,-1,-1,-1 ] #unknown size means infinte size array as explained earlier in line 29
+arr = [ 1,2,3,4,5,6,7,8,9,-1,-1,-1,-1,-1 ] #to find the position of the key in   infinte size array as explained earlier in line 29
 ans = findPos(arr,8)
 if ans == -1:
     print ("Element not found")
